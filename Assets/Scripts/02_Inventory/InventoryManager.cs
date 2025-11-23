b9f1837821b22e28735b1dcdc9cc6935b5c41fc3
megumi <3055717975@qq.com>
2025-11-23 19:15:04 +0800

    feat:实现背包基础数据结构与管理器

---
+++ Assets/Scripts/02_Inventory/InventoryManager.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// --- 1. 定义背包格子类 ---
+[System.Serializable]
+public class InventorySlot
+{
+    public ItemData itemData;           //存那种物品 
+    public int amount;                  //存了多少个
+
+    //构造函数
+    public InventorySlot( ItemData data, int count)
+    {
+        itemData = data;
+        amount = count;
+    } 
+
+    //增加数量
+    public void AddAmount( int count )
+    {
+        amount += count;
+    }
+}
+
+// --- 2. 背包管理器 ---
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //背包容器，就是一个格子的列表
+    //使用列表方便有序的显示
+    public List<InventorySlot> backpack = new List<InventorySlot>();
+
+    
+    //核心逻辑；添加物品
+    public void AddItem(ItemData data, int count)
+    {
+        //检查背包中是否存在该物品
+        //只有可堆叠的物品才需要检查
+        InventorySlot foundSlot = null;
+
+        
+        if ( data.isStackable )
+        {
+            //遍历List查找
+            foundSlot = backpack.Find( slot => slot.itemData = data );
+        }
+
+        //如果该物品已存在
+        if ( foundSlot != null )
+        {
+            //物品已存在且不可堆叠，直接增加数量
+            foundSlot.AddAmount( count );
+            Debug.Log($"[背包] {data.itemName} 数量增加，当前: {foundSlot.amount}");
+        }
+        else
+        {
+            //物品不存在或者不可堆叠，创建一个新的格子
+            InventorySlot newSlot = new InventorySlot( data, count );
+            backpack.Add( newSlot );
+            Debug.Log($"[背包] 获得新物品: {data.itemName} x{count}");
+        }
+    }
+
+    // 仅用于测试，正式上线要删掉
+    public ItemData testItem; // 把你做好的 ScriptableObject 拖进这里
+
+    private void Update()
+    {
+        if( Input.GetKeyDown(KeyCode.Space))
+        {
+            AddItem(testItem, 1);
+        }
+    }
+}
---
+++ Assets/Scripts/02_Inventory/ItemData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/ItemData")]
+public class ItemData : ScriptableObject
+{
+    [Header("物品基础信息")]
+    public int itemID;          // 唯一ID (用于字典查找)
+    public string itemName;     // 名字
+    public Sprite icon;         // 图标 (UI显示用)
+
+    [TextArea]
+    public string description;  // 描述
+
+    [Header("物品属性")]
+    public bool isStackable;    // 是否可堆叠
+    public int maxStackSize = 1; // 最大堆叠数
+}
+    
